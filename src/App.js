51aa928e82b5fb2702521fd3a0ad603983b323fa
Petzlc <chlodwig.petzl@hotmail.com>
2024-10-14 16:25:50 +0100

    Implement API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 export default function App() {
   // set state for the input in the searchbar
   const [searchQuery, setSearchQuery] = useState('');
 
+  // state for the results
+  const [results, setResults] = useState([]);
+
   // implement search function later
-  const handelSearch = () => {
-    console.log('Suche nach:', searchQuery);
+  const handelSearch = async () => {
+    // console.log('Suche nach:', searchQuery);
+    try {
+      const response = await axios.get(
+        'https://marketplace.sshopencloud.eu/item-search',
+        {
+          params: {
+            q: searchQuery,
+            categories: 'tool-or-service',
+          },
+        },
+      );
+      // Put results in State
+      setResults(response.data.items);
+    } catch (error) {
+      console.error('Fehler bei der API-Anfrage:', error);
+    }
   };
 
   return (
     <div>
       <h1>Social Sciences & Humanities Open Marketplace</h1>
 
-      {/* Suchfeld */}
+      {/* Search field */}
       <div>
         <input
           type="text"
@@ -25,13 +44,19 @@ export default function App() {
         <button onClick={handelSearch}>Suche</button>
       </div>
 
-      {/* Ergebnisliste */}
+      {/* Results list */}
       <div>
         <h2>Ergebnisse</h2>
         <ul>
-          <li>Ergebnis 1</li>
-          <li>Ergebnis 2</li>
-          <li>Ergebnis 3</li>
+          {results.map((item) => (
+            <li key={item.id}>
+              <strong>{item.label}</strong>
+              <br />
+              Accessible at: {item.accessibleAt}
+              <br />
+              Contributors: {item.contributors.join(', ')}
+            </li>
+          ))}
         </ul>
       </div>
 
