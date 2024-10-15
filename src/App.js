e6788e40398b1d89530406cc1a6dd1beebc61fe4
Petzlc <chlodwig.petzl@hotmail.com>
2024-10-15 11:50:28 +0100

    Implement Pagination with page amounts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ export default function App() {
   // state for the results
   const [results, setResults] = useState([]);
 
+  // current page
+  const [page, setPage] = useState(1);
+
+  // page size or number of items per page
+  const [pageSize, setPageSize] = useState(10);
+
+  // Total amount of pages
+  const [totalPages, setTotalPages] = useState(1);
+
   // implement search function later
   const handelSearch = async () => {
     // console.log('Suche nach:', searchQuery);
@@ -15,16 +24,37 @@ export default function App() {
       const response = await axios.get(
         'https://marketplace-api.sshopencloud.eu/api/item-search',
         {
+          // Parameters as stated inthe assessment
           params: {
             q: searchQuery,
             categories: 'tool-or-service',
+            page: page,
+            pageSize: pageSize,
           },
         },
       );
       // Put results in State
       setResults(response.data.items);
+
+      // calculate total amount of pages
+      setTotalPages(Math.ceil(response.data.totalItems / pageSize));
+
+      // test in console
+      console.log('Results:', response.data.items);
     } catch (error) {
       console.error('Fehler bei der API-Anfrage:', error);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (page < totalPages) {
+      setPage(page + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
     }
   };
 
@@ -62,9 +92,15 @@ export default function App() {
 
       {/* Pagination */}
       <div>
-        <button>{'<'}</button>
-        <span>Page 1 of 10</span>
-        <button>{'>'}</button>
+        <button onClick={handlePreviousPage} disabled={page === 1}>
+          {'<'}
+        </button>
+        <span>
+          Page {page} of {totalPages}
+        </span>
+        <button onClick={handleNextPage} disabled={page === totalPages}>
+          {'>'}
+        </button>
       </div>
     </div>
   );
