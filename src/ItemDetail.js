146b8ec122206ec37526c04bf59a3ddb039f99d3
Petzlc <chlodwig.petzl@hotmail.com>
2024-10-16 15:20:51 +0100

    Implement ItemDatails page

---
+++ src/ItemDetail.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+export default function ItemDetail() {
+  return (
+    <div>
+      <h1>This is the page for the ItemDetails</h1>
+    </div>
+  );
+}
